3f4ca56bbb548484db6a2079605179da5a5eeafb
jelle89 <51077942+jelle89@users.noreply.github.com>
2019-06-01 12:59:00 +0200

    step1
    working on step 1

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,24 @@
 // Write your JS here
+var hero = {
+    name: "piet",
+    heroic: true,
+    health: 10,
+    inventory: [],
+    weapon: {
+        type: 'sword',
+        damage: 2
+    }
+}
+
+
+function rest(hero) {
+    hero.health = 10
+        if (hero.health == 10){
+         alert('health is already 10')
+         return(hero)
+}
+
+}
+
+function pickUpItem() {}
+function equipWeapon() {}
